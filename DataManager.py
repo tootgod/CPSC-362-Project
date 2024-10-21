660758f2e2b203e7807c7feaff91fe3b717fdea5
tootgod <geesinkk@gmail.com>
2024-10-21 08:56:20 -0700

    Added Comments to DataManager.py and Main.py Not sure if this was really necessary, but I did it anyway. Most of it is pretty self-explanatory.S

--- DataManager.py
+++ DataManager.py
@@ -3,36 +3,44 @@ import json
 from datetime import datetime, timedelta
 import pandas as pd
 
+#downloads FNGU data and saves it to a json file with a set start range
 def downloadFNGU():
     today = datetime.now()
     end_date = today.strftime('%Y-%m-%d')
     fngu_data = yf.download('FNGU', start='2020-01-01', end=end_date)
     fngu_data.to_json('historical_data.json')
 
+#downloads FNGD data and saves it to a json file with a set start range
 def downloadFNGD():
     today = datetime.now()
     end_date = today.strftime('%Y-%m-%d')
     fngd_data = yf.download('FNGD', start='2020-01-01', end=end_date)
     fngd_data.to_json('historical_data.json')
 
+#loads the json data from the file
 def loadData():
     return json.load(open('historical_data.json', 'r'))
 
+#returns a list of dates from the json data in Unix time
 def getJsonDates(data):
    return [int(date) / 1000 for date in data['Close'].keys()]
 
+#returns a list of daily closing prices from the json data
 def getJsonCloses(data):
     lists = list(data['Close'].values())
     return lists
 
+#returns a list of daily highs from the json data
 def getJsonHighs(data):
     lists = list(data['High'].values())
     return lists
 
+#returns a list of daily lows from the json data
 def getJsonLows(data):
     lists = list(data['Low'].values())
     return lists
 
+#returns a list of daily opens from the json data
 def getJsonOpens(data):
     lists = list(data['Open'].values())
     return lists
