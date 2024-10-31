a8bee6d938e97ae27256905f2e6224f7a7b1b1d8
fehojaf <joon.rho233@gmail.com>
2024-10-30 20:36:17 -0700

    Update BBbacktest.py

--- BBbacktest.py
+++ BBbacktest.py
@@ -3,24 +3,30 @@ import csv
 import SmaBacktest as sma
 import DataManager as dm
 
-def std_deviation(mean, date, period)
+def upper_lower(std_d, mid_line):
+    for i in range(len(mid_line)):
+        upper = mid_line[i] + (std_d[i] * 2)
+        lower = mid_line[i] - (std_d[i] * 2)
     
-    return result
 
 def BB_backtest(historical_data, *, period=20, symbol, initial_balance=100000):
     dates = dm.getJsonDates(historical_data)
     closes = dm.getJsonCloses(historical_data)
 
-  
+    # Convert to pandas dataframe
     df = pd.DataFrame({
         'Date': pd.to_datetime(dates),
         'Close': closes
     })
-  
-    df["MiddleLine"] = sma.calculate_sma(df['Close'], period)
 
+    # Get middle line and standard deviations of each period
+    df["MiddleLine"] = sma.calculate_sma(df['Close'], period)
+    df["StdDeviation"] = df["MiddleLine"].rolling(window=period).std()
     
-  
+    
+    # Backtest variables
     balance = initial_balance
     shares_held = 0
     trade_log = []
+    
+        
